c6f26591accfefeba0d34ee31ced17e1498d8f40
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-09-02 11:41:33 -0400

    Done

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -9,15 +9,20 @@ func downloadImage(atURL url: String) -> (Bool, String) {
 let result = downloadImage(atURL: "http://example.com/image.png")
 result.0
 result.1
+
 print("Success? \(result.0)")
 print("Message: \(result.1)")
 
-let person = (name: "Jim", age: 30)
+let person = (name:"Jim", age:30)
+
 person.age
 person.name
-print("\(person.name)'s age is \(person.age)")
 
-var person2 = (name: "Jim", age: 30)
-print("\(person2.name)'s age is \(person2.age)")
-person2.age = 32
-print("\(person2.name)'s age is \(person2.age)")
+print("You are \(person.age) years old.")
+print("You are \(person.name)!")
+
+var person2 = (name:"Jim", age:"32")
+person2.age
+person2.name
+
+print("You are \(person2.name) and \(person2.age) years old.")
